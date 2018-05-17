9e760c7e0d3167b98713d779b283f9daa55359ba
sivaranjiniR <36620039+sivaranjiniR@users.noreply.github.com>
2018-05-17 09:40:16 +0530

    Create largofthree.py

---
+++ beginner level/largofthree.py
@@ -0,0 +1,12 @@
+s=int(raw_input())
+r=int(raw_input())
+l=int(raw_input())
+if(s>=r) and (s>=l):
+		print 'OUTPUT'
+		print s
+elif(r>=s) and (r>=l):
+		print 'OUTPUT'
+		print r
+else:
+		print 'OUTPUT'
+		print l
