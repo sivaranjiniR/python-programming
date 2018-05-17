1bc9125d6f53bce63ecd883a38aa84c2026cf965
sivaranjiniR <36620039+sivaranjiniR@users.noreply.github.com>
2018-05-17 10:14:15 +0530

    Update largofthree.py

--- beginner level/largofthree.py
+++ beginner level/largofthree.py
@@ -1,12 +1,10 @@
-s=int(raw_input())
-r=int(raw_input())
-l=int(raw_input())
+s,r,l=[int(x) for x in raw_input().split()]
 if(s>=r) and (s>=l):
-		print 'OUTPUT'
-		print s
+	print 'OUTPUT'
+	print s
 elif(r>=s) and (r>=l):
-		print 'OUTPUT'
-		print r
+	print 'OUTPUT'
+	print r
 else:
-		print 'OUTPUT'
-		print l
+	print 'OUTPUT'
+	print l
